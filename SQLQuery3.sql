f1a21e8da29973f2631d2e5cc87aeb5e10d3a4dd
Vin-2709 <vin.itku7277@gmail.com>
2025-10-20 14:32:03 +0530

    Remove duplicate SQL queries from SQLQuery3.sql

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -58,29 +58,6 @@ WHERE
 
 
 
-WITH DuplicateRecords AS (
-    SELECT 
-        JourneyID,  
-        CustomerID,  
-        ProductID,  
-        VisitDate,  
-        Stage,  
-        Action,  
-        Duration,  
-        ROW_NUMBER() OVER (
-            PARTITION BY CustomerID, ProductID, VisitDate, Stage, Action  
-            ORDER BY JourneyID  
-        ) AS row_num  
-    FROM 
-        dbo.customer_journey  
-)
-    
-SELECT *
-FROM DuplicateRecords
-ORDER BY JourneyID;  
-
-
-
 SELECT 
     JourneyID,  
     CustomerID,  
@@ -109,3 +86,4 @@ FROM
     ) AS subquery  
 WHERE 
     row_num = 1;
+
